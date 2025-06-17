997472f90fd02be8c1bca7be4c6889cdac8efad5
Steve Aldworth <s.aldworth@tvdsb.ca>
2025-06-17 12:37:10 -0400

    Add project files.

---
+++ 3U Methods/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Xml.Serialization;
+
+namespace _3U_Methods
+{
+    // Enter code here
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double itemPrice = 100;
+            double tax = calculateTax(itemPrice);
+            Console.WriteLine("Tax is: $" + tax);
+        }
+
+        public static double Square(double number)
+        {
+            return number * number;
+        }
+
+
+        public static void DrawCheck()
+        {
+            Console.WriteLine("     /");
+            Console.WriteLine("    /");
+            Console.WriteLine("\\  /"); // Since '\' is a special character, we must use two of them to print one
+            Console.WriteLine(" \\/");
+        }
+
+        public static void DrawX()
+        {
+            Console.WriteLine("\\  /");
+            Console.WriteLine(" \\/");
+            Console.WriteLine(" /\\"); // Since '\' is a special character, we must use two of them to print one
+            Console.WriteLine("/  \\");
+        }
+
+        public static double calculateTax(double price)
+        {
+            const double taxRate = 0.13;
+            return price * taxRate;
+        }
+
+        public static double FinalPrice(double price)
+        {
+            return price + calculateTax(price); // This method is simple enough that you could just calclate the tax directly here, but it's better to keep it separate for clarity and reusability.
+        }
+    }
+}
